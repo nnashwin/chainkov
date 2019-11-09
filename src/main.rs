4194b4c0ba1550a8c6a2999fbdf1c9a43b563437
Tyler Boright <ru.lai.development@gmail.com>
2019-11-08 22:01:55 -0800

    modifies next_state to handle situations where the probability for a specific key is not in the markov chain; adds a generate_states function that can run through the chain many times and return the sequence and unit tests for both

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,6 @@ struct MarkovChain {
 }
 
 impl MarkovChain {
-    fn next_state(&mut self, current_state: String) -> String {
-        let probabilities = &self.transition_prob.get(&current_state).unwrap();
-
-        return probabilities
-            .choose_weighted(&mut thread_rng(), |state_prob| state_prob.1)
-            .unwrap()
-            .0
-            .clone();
-    }
-
-    fn new() -> MarkovChain {
-        MarkovChain {
-            transition_prob: HashMap::new(),
-            states: Vec::new(),
-        }
-    }
-
     fn add_state_choice(&mut self, key: &str, probability: (String, f32)) {
         if self.transition_prob.contains_key(key) {
             // write logic to replace probability if it already exists in the Vector
@@ -47,22 +30,116 @@ impl MarkovChain {
             self.transition_prob.insert(key.to_string(), prob_vec);
         }
     }
+
+    fn generate_states(&self, mut current_state: String, num_of_states: u16) -> Vec<String> {
+        let mut future_states: Vec<String> = vec![];
+        
+        for _ in 0..num_of_states {
+            let next_state = self.next_state(current_state.to_string());
+            future_states.push(next_state.clone());
+            current_state = next_state;
+        }
+        return future_states;
+    }
+
+    fn new() -> MarkovChain {
+        MarkovChain {
+            transition_prob: HashMap::new(),
+            states: Vec::new(),
+        }
+    }
+
+    fn next_state(&self, current_state: String) -> String {
+        let probabilities = if self.transition_prob.contains_key(&current_state) {
+            self.transition_prob.get(&current_state)
+        } else {
+            None
+        };
+
+        match probabilities {
+            Some(x) => { 
+                return x
+                    .choose_weighted(&mut thread_rng(), |state_prob| state_prob.1)
+                    .unwrap()
+                    .0
+                    .clone();
+
+            },
+            None => "".to_string(),
+        }
+    }
 }
 
 fn main() {
     let mut m_chain = MarkovChain::new();
+    m_chain.next_state("a".to_string());
     m_chain.add_state_choice("a", ("c".to_string(), 0.8));
     println!("{:?}", m_chain.transition_prob);
     m_chain.add_state_choice("a", ("b".to_string(), 0.8));
     println!("{:?}", m_chain.transition_prob);
-    m_chain.add_state_choice("a", ("b".to_string(), 0.6));
+    m_chain.add_state_choice("b", ("a".to_string(), 0.6));
     println!("{:?}", m_chain.transition_prob);
-    m_chain.add_state_choice("a", ("a".to_string(), 0.9));
+    m_chain.add_state_choice("c", ("b".to_string(), 0.9));
     println!("{:?}", m_chain.transition_prob);
-    m_chain.add_state_choice("a", ("d".to_string(), 0.6));
+    m_chain.add_state_choice("b", ("c".to_string(), 0.6));
     println!("{:?}", m_chain.transition_prob);
-    m_chain.add_state_choice("a", ("f".to_string(), 0.9));
+    m_chain.add_state_choice("b", ("b".to_string(), 0.9));
     println!("{:?}", m_chain.transition_prob);
     m_chain.add_state_choice("a", ("c".to_string(), 0.4));
-    println!("{:?}", m_chain.transition_prob);
+    m_chain.next_state("a".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let m = MarkovChain::new();
+        let expected = MarkovChain {
+            transition_prob: HashMap::new(),
+            states: vec![],
+        };
+
+        assert_eq!(m.transition_prob, expected.transition_prob);
+    }
+
+    #[test]
+    fn test_add_state_choice() {
+        let mut m = MarkovChain::new();
+        m.add_state_choice("a", ("c".to_string(), 0.8));
+        m.add_state_choice("a", ("b".to_string(), 0.19));
+        m.add_state_choice("a", ("a".to_string(), 0.01));
+
+        let mut expected_prob_vec = vec![];
+        expected_prob_vec.push(("c".to_string(), 0.8));
+        expected_prob_vec.push(("b".to_string(), 0.19));
+        expected_prob_vec.push(("a".to_string(), 0.01));
+
+        let mut expected_hash_map = HashMap::new();
+        expected_hash_map.insert("a", expected_prob_vec);
+
+        assert_eq!(m.transition_prob.get("a"), expected_hash_map.get("a"));
+    }
+
+    #[test]
+    fn test_next_state() {
+        let mut m = MarkovChain::new(); 
+        m.add_state_choice("a", ("b".to_string(), 1.0));
+        m.add_state_choice("a", ("c".to_string(), 0.0));
+
+        assert_eq!(m.next_state("a".to_string()), "b");
+        assert!(m.next_state("a".to_string()) != "c");
+    }
+
+    #[test]
+    fn test_generate_states() {
+        let mut m = MarkovChain::new(); 
+        m.add_state_choice("a", ("b".to_string(), 1.0));
+        m.add_state_choice("b", ("c".to_string(), 1.0));
+        m.add_state_choice("c", ("a".to_string(), 1.0));
+
+        assert_eq!(m.generate_states("a".to_string(), 6), vec!["b", "c", "a", "b", "c", "a"]);
+        assert!(m.next_state("a".to_string()) != "c");
+    }
 }
